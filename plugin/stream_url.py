f128b8305961441bb17e834974a05a7a07f36047
Garulf <535299+Garulf@users.noreply.github.com>
2021-11-29 00:51:41 -0500

    init

---
+++ plugin/stream_url.py
@@ -0,0 +1,12 @@
+from flox import Flox
+
+class StreamUrl(Flox):
+
+    def query(self, query):
+        pass
+
+    def context_menu(self, data):
+        pass
+
+if __name__ == "__main__":
+    StreamUrl()
